8ad0163ff2dd8493b9f56a405324f59058333685
Jhonel Villanueva <vvjhonel@gmail.com>
2022-12-14 03:21:40 +0800

    Fix button functionality

--- script.js
+++ script.js
@@ -17,26 +17,51 @@ const divide = (num1, num2) => {
 };
 
 const operate = (operator, num1, num2) => {
-  return operator(num1, num2);
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case 'x':
+      return multiply(num1, num2);
+    case '÷':
+      return divide(num1, num2);
+    default:
+      return;
+  }
 };
 
 // * Calculator
 
-const display = document.querySelector('.display');
+const mainDisplay = document.querySelector('.main-display');
+const secondDisplay = document.querySelector('.second-display');
 const buttons = document.querySelectorAll('button');
+mainDisplay.textContent = '0';
 
 let currentDisplay = '';
+let firstDigit = null;
+let secondDigit = null;
+let operator = '';
+let isFirstDigit = true;
 
 const inputDigit = (input) => {
-  if (currentDisplay.length === 11) return;
+  if (currentDisplay.length >= 11) return;
+  if (input === '0' && currentDisplay === '0') return;
+  if (input === '.' && currentDisplay.includes('.')) return;
+  if (input === '.' && currentDisplay === '') {
+    currentDisplay = '0';
+  }
+  if (input !== '.' && currentDisplay === '0') {
+    currentDisplay = '';
+  }
 
   currentDisplay += input;
-  display.textContent = currentDisplay;
-  console.log(input);
+  mainDisplay.textContent = currentDisplay;
 };
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
+    e.preventDefault();
     inputDigit(e.target.value);
   });
 });
