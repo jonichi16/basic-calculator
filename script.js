ba11380920a80a971dae6cf4a9a15163ce85dee1
Jhonel Villanueva <vvjhonel@gmail.com>
2022-12-13 14:20:48 +0800

    Add button functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// * Operators
+
 const add = (num1, num2) => {
   return num1 + num2;
 };
@@ -17,3 +19,24 @@ const divide = (num1, num2) => {
 const operate = (operator, num1, num2) => {
   return operator(num1, num2);
 };
+
+// * Calculator
+
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+let currentDisplay = '';
+
+const inputDigit = (input) => {
+  if (currentDisplay.length === 11) return;
+
+  currentDisplay += input;
+  display.textContent = currentDisplay;
+  console.log(input);
+};
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    inputDigit(e.target.value);
+  });
+});
